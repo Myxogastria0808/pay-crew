60239ed162fd1e031b7f610c9f383c459b15b65e
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-10-01 16:10:40 +0900

    wip

--- products/backend/src/tmp/index.ts
+++ products/backend/src/tmp/index.ts
@@ -6,12 +6,28 @@ type ResponsesConfig = Partial<Record<StatusCode, ResponsesEntry>>;
 /** ユーザーが渡したマップ M に実在するステータスコードだけを抽出 */
 type UserDefinedStatusCode<M extends ResponsesConfig> = Extract<StatusCode, keyof M>;
 
+/** タプル T が重複を含んでいたら never、そうでなければ T をそのまま返す */
+type UniqueTuple<
+  Elm extends UserDefinedStatusCode<ResponsesConfig>,
+  T extends Readonly<Elm[]>,
+  Seen extends Readonly<Elm[]> = [],
+> =
+  T extends Readonly<[infer Head extends Elm, ...infer Tail extends Elm[]]>
+    ? Head extends Seen[number]
+      ? never
+      : Readonly<[Head, ...UniqueTuple<Elm, Tail, [...Seen, Head]>]>
+    : T;
+
 export class ZodOpenAPISchema<M extends ResponsesConfig> {
   private responses: Readonly<M>;
   constructor(responses: M) {
     this.responses = responses;
   }
-  createSchema<R extends RouteConfig, T extends Readonly<UserDefinedStatusCode<M>[]>>(route: R, statusCodes?: T): R {
+
+  createSchema<R extends RouteConfig, T extends Readonly<UserDefinedStatusCode<M>[]>>(
+    route: R,
+    statusCodes?: UniqueTuple<UserDefinedStatusCode<M>, T>
+  ): R {
     if (statusCodes) {
       const extraResponses: ResponsesConfig = {};
       for (const statusCode of statusCodes) {
