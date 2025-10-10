f15f60fa8a12f6193ac5e7da58755a2ba999df10
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-10-11 00:00:05 +0900

    wip

--- products/backend/src/presentation/share/index.ts
+++ products/backend/src/presentation/share/index.ts
@@ -1,13 +1,13 @@
-import { errorResponseAPISchema } from 'paycrew-validator';
+import { errorResponseSchema } from 'paycrew-validator';
 import { ZodOpenAPISchema } from 'zod-openapi-share';
 
 export const route = new ZodOpenAPISchema({
   400: {
     description: 'Bad Request',
-    content: { 'application/json': { schema: errorResponseAPISchema } },
+    content: { 'application/json': { schema: errorResponseSchema } },
   },
   500: {
     description: 'Internal Server Error',
-    content: { 'application/json': { schema: errorResponseAPISchema } },
+    content: { 'application/json': { schema: errorResponseSchema } },
   },
 } as const);
