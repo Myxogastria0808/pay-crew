c362640904bddac8b96acb9a8d4965d52d6fccfb
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-10-31 17:28:03 +0900

    wip

--- products/frontend/eslint.config.js
+++ products/frontend/eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
@@ -11,13 +11,15 @@ export default defineConfig([
   },
   js.configs.recommended,
   tseslint.configs.recommended,
-  reactHooks.configs['recommended-latest'],
-  reactRefresh.configs.vite,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: '2020',
+      ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
   },
 ]);
