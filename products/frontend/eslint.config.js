a0955673c9d1ebcd6691b03c6d4c7894dee9f4c3
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-10-31 17:16:28 +0900

    chore: update eslint config

--- products/frontend/eslint.config.js
+++ products/frontend/eslint.config.js
@@ -5,21 +5,19 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig(
+export default defineConfig([
   {
-    ignores: ['dist', '.turbo/', 'coverage', 'dist', 'html', 'node_modules', 'public'],
+    ignores: ['dist', '.turbo/', 'coverage', 'html', 'node_modules', 'public'],
   },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: '2020',
       globals: globals.browser,
     },
-  }
-);
+  },
+]);
