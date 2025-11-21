c0c6ea90209ace2d195b844cd77afcb22470452a
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-11-21 19:09:08 +0900

    wip

--- products/notify/src/reminder.ts
+++ products/notify/src/reminder.ts
@@ -6,6 +6,10 @@ export const reminder = async (api_url: string): Promise<string> => {
     headers: { 'Content-Type': 'application/json' },
   });
 
+  if (!response.ok) {
+    throw new Error('Failed to fetch history data');
+  }
+
   const historyData = await response.json<HistoryGetResponseSchemaType>();
 
   const message = historyData.map((v) => `返金の流れ: ${v.to} -> ${v.from}\n\t金額: ${v.amount}\n`).join('\n');
