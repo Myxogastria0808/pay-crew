086ca19073f06f3973bdab247e05facac147d8a2
Redaction-F <koebijakobiteorhang@gmail.com>
2025-11-21 16:26:42 +0900

    notify reminder

---
+++ products/notify/src/reminder.ts
@@ -0,0 +1,22 @@
+type historyDatasType = {
+  id: number;
+  from: string;
+  to: string;
+  amount: number;
+}[];
+
+export const reminder = async () => {
+  const discordWebhookUrl = "https://discord.com/api/webhooks/1430405385671671858/EZZlF3vrhVw-zwhBg9OVVuINsOJHSc-NneYRfVKzR-V32Ng76lYLcByOnVKCkNuVrIfG";
+  const historyApiUrl = "http://localhost:8787/api/history";
+
+  const response = await fetch(historyApiUrl, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const historyData = await response.json<historyDatasType>();
+
+  const message = historyData.map((v) => `返金の流れ: ${v.to} -> ${v.from}\n\t金額: ${v.amount}\n`).join("\n");
+
+  return `========================================\n現在残っている返金\n${message}\n========================================`;
+};
