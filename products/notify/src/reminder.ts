633b7fe1479d4249c035c2c04819bfd86b8418e6
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-11-21 16:42:08 +0900

    chore: add db fetch utils

--- products/notify/src/reminder.ts
+++ products/notify/src/reminder.ts
@@ -1,22 +1,18 @@
-type historyDatasType = {
-  id: number;
-  from: string;
-  to: string;
-  amount: number;
-}[];
+import { type HistoryGetResponseSchemaType } from 'validator';
 
-export const reminder = async () => {
-  const discordWebhookUrl = "https://discord.com/api/webhooks/1430405385671671858/EZZlF3vrhVw-zwhBg9OVVuINsOJHSc-NneYRfVKzR-V32Ng76lYLcByOnVKCkNuVrIfG";
-  const historyApiUrl = "http://localhost:8787/api/history";
+export const reminder = async (): Promise<string> => {
+  const discordWebhookUrl =
+    'https://discord.com/api/webhooks/1430405385671671858/EZZlF3vrhVw-zwhBg9OVVuINsOJHSc-NneYRfVKzR-V32Ng76lYLcByOnVKCkNuVrIfG';
+  const historyApiUrl = 'https://pay-crew-api.yukiosada.work/api/history';
 
   const response = await fetch(historyApiUrl, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
   });
 
-  const historyData = await response.json<historyDatasType>();
+  const historyData = await response.json<HistoryGetResponseSchemaType>();
 
-  const message = historyData.map((v) => `返金の流れ: ${v.to} -> ${v.from}\n\t金額: ${v.amount}\n`).join("\n");
+  const message = historyData.map((v) => `返金の流れ: ${v.to} -> ${v.from}\n\t金額: ${v.amount}\n`).join('\n');
 
   return `========================================\n現在残っている返金\n${message}\n========================================`;
 };
